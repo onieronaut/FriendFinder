3aa31b67860ca0e2d1a7d04e0580528242d033c8
jerard <qbin221@gmail.com>
2019-12-19 16:39:34 -0500

    completed api routes, created function to select the best match, added modal to display best match

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,39 @@
-let friends = [];
+let friends = [
+    {
+        name: "Billy Bob",
+        photo: "No",
+        scores: [2,4,1,3,5,5,5,4,2,3]
+    },
+    {
+        name: "Chad",
+        photo: "No",
+        scores: [2,3,1,2,4,1,5,5,2,3]
+    },
+    {
+        name: "Brad",
+        photo: "No",
+        scores: [2,4,5,1,5,2,5,3,2,3]
+    },
+    {
+        name: "Thad",
+        photo: "No",
+        scores: [2,1,1,3,4,5,2,2,2,2]
+    },
+    {
+        name: "Dad",
+        photo: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        scores: [2,2,1,1,1,1,1,2,2,2]
+    },
+    {
+        name: "Mad",
+        photo: "No",
+        scores: [2,4,1,3,5,1,4,5,4,1]
+    },
+    {
+        name: "Dad",
+        photo: "hello",
+        scores: [2,2,1,3,4,5,4,4,2,1]
+    }
+];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,52 @@
 const friends = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
+
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function (req, res) {
+
+        let newUser = req.body;
+
+        friends.push(req.body);
+
+        let bestMatch = findBestMatch(newUser, friends);
+        
+        res.json(friends[bestMatch]);
+
+    });
+}
+
+function findBestMatch(user, matches) {
+
+    let userScores = user.scores
+
+    let bestMatch;
+
+    let leastDifference = 41;
 
     
+    for (let i = 0; i < matches.length; i++) {
+        
+        let totalDifference = 0;
+
+        for (let j = 0; j < 10; j++) {
+
+            totalDifference += Math.abs(userScores[j] - matches[i].scores[j])
+
+        }
+
+        if (totalDifference < leastDifference && user.photo !== matches[i].photo) {
+
+            leastDifference = totalDifference;
+            bestMatch = i;
+            
+        }
+        
+    }
+    
+    return bestMatch;
 }
