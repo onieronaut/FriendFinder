4225b21e0cf21fbc90bbf7bbd25a8b5f84d8e100
jerard <qbin221@gmail.com>
2020-01-07 18:20:33 -0500

    added json character

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,10 @@
 // Empty array to store our user object information as they are created
-let friends = [];
+let friends = [
+    {
+        name: "Baby Yoda",
+        photo: "https://images2.minutemediacdn.com/image/upload/c_crop,h_1224,w_2177,x_80,y_0/f_auto,q_auto,w_1100/v1574876645/shape/mentalfloss/609512-disney_0.jpg",
+        scores: [5, 4, 3, 2, 1, 5, 4, 3, 2, 1]
+    }
+];
 
 module.exports = friends;
